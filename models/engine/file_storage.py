31f4748957cc15088e1bf4651beca327c21b7c95
ahlemkaabi <ahlemkaabi1412@gmail.com>
2021-06-28 15:26:08 +0100

    file storage attribute error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,18 +5,24 @@
 """
 
 import json,  os.path
+from os import closerange
 class FileStorage:
+    """ __file_path:
+        __objects:
+            -dictionary of objects: key=id + value=object
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """Returns: the dictionary __objects """
+        # print(FileStorage.__objects)
         return FileStorage.__objects
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         obj_dict = obj.to_dict()
-        key = "{}.{}".format( obj_dict[__class__], obj_dict[id])
+        key = "{}.{}".format( obj_dict["__class__"], obj_dict["id"])
         FileStorage.__objects[key] = obj
 
 
@@ -31,10 +37,10 @@ class FileStorage:
         my_dict_elements = {}
         for key, value in FileStorage.__objects.items():
             dict_value = value.to_dict()
+            #print(type(dict_value["updated_at"]))
             my_dict_elements.update({key: dict_value})
-
         with open('{}'.format(FileStorage.__file_path), 'w') as file:
-            file.write(json.dump(my_dict_elements))
+            json.dump(my_dict_elements, file)
 
 
     def reload(self):
@@ -43,6 +49,8 @@ class FileStorage:
             -If the file doesn’t exist, no exception should be raised)
         """
         if os.path.exists('{}'.format(FileStorage.__file_path)) == True:
-            my_dict_elements = json.load('{}'.format(FileStorage.__file_path))
-            for key, value in my_dict_elements.items():
-                FileStorage.__objects.update({key: json.loads(value)})
+            with open('{}'.format(FileStorage.__file_path), 'r', encoding="UTF-8") as file:
+                json_data = json.load(file)
+                print(type(json_data))
+            for key, value in json_data.items():
+                FileStorage.new(self, value)
